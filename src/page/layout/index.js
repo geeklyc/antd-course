d62c26ad925669ce7ff9032bb66d6ed477b0a1c8
liyoucheng <geeklyc@163.com>
2019-07-11 17:22:09 +0800

    侧边导航

--- src/page/layout/index.js
+++ src/page/layout/index.js
@@ -1,5 +1,6 @@
 import { Component } from 'react'
-import { Layout } from 'antd'
+import { Layout, Menu, Icon } from 'antd'
+import SubMenu from 'antd/lib/menu/SubMenu';
 
 const { Header, Footer, Sider, Content } = Layout;
 
@@ -8,12 +9,26 @@ export default class BasicLayout extends Component {
         return (
             <Layout>
                 <Sider width={256} style={{ minHeight: '100vh', color: 'white' }}>
-                    Sider
+                    <div style={{ height: '32px', background: 'rgba(255,255,255,.2)', margin: '16px'}}/>
+                    <Menu theme="dark" mode="inline" defaultSelectedKeys={['1']}>
+                        <Menu.Item key="1">
+                            <Icon type="pie-chart"/>
+                            <span>Helloworld</span>
+                        </Menu.Item>
+                        <SubMenu 
+                        key="sub1" 
+                        title={<span><Icon type="dashboard" /><span>Dashboard</span></span>}>
+                            <Menu.Item key="2">分析页</Menu.Item>
+                            <Menu.Item key="3">监控页</Menu.Item>
+                            <Menu.Item key="4">工作台</Menu.Item>
+                        </SubMenu>
+                    </Menu>
                 </Sider>
                 <Layout>
                     <Header style={{ background: '#fff', textAlign: 'center', padding: 0}}>
                         Header
                     </Header>
+                    //上右下左
                     <Content style={{ margin: '24px 16px 0' }}>
                         <div style={{ padding: 24, background: '#fff', minHeight: 360}}>
                             {this.props.children}
