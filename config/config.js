86133423192047647ea4537684fbcdb180b01df1
liyoucheng <geeklyc@163.com>
2019-07-11 19:00:01 +0800

    路由配置

--- config/config.js
+++ config/config.js
@@ -8,11 +8,23 @@ export default {
   ],
   routes: [{
     path: '/',
-    component: './layout',
+    component: '../layout',
     routes: [
       {
-        path: 'helloworld',
-        component: './HelloWorld'
+        path: '/',
+        component: 'Helloworld',
+      },
+      {
+        path: '/helloworld',
+        component: 'Helloworld'
+      },
+      {
+        path: '/dashboard',
+        routes: [
+          { path: '/dashboard/analysis', component: 'Dashboard/Analysis' },
+          { path: '/dashboard/monitor', component: 'Dashboard/Monitor' },
+          { path: '/dashboard/workplace', component: 'Dashboard/Workplace' }
+        ]
       },
     ]
   }],
---
+++ src/page/App.js
@@ -0,0 +1,5 @@
+export default (props) => {
+    <div style={{ padding: 20 }}>
+        {this.props.children}
+    </div>
+}
